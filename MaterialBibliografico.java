9e4264d7dc4c8b7545ecf58554a12d780c525850
Iris <irisale.ayala@gmail.com>
2023-10-05 07:26:52 -0600

    Creacion de atributos getters y setters

---
+++ MaterialBibliografico.java
@@ -0,0 +1,32 @@
+public class MaterialBibliografico {
+    String nombre;
+    String editorial;
+    Integer año_publicacion;
+    String genero;
+    String autor;
+    String tipo;
+    
+    public void generarReferencia(){
+
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+    public String getEditorial(){
+        return editorial;
+    }
+    public Integer getAño(){
+        return año_publicacion;
+    }
+    public String getGenero(){
+        return genero;
+    }
+    public String getAutor(){
+        return autor;
+    }
+
+    public String getTipo(){
+        return tipo;
+    }
+}
