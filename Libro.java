fb86941a71cb414bbfae6974b583b825f28357a4
Iris <irisale.ayala@gmail.com>
2023-10-05 16:51:45 -0600

    se implemento la referencia para esta clase

--- Libro.java
+++ Libro.java
@@ -1,11 +1,11 @@
 public class Libro extends MaterialBibliografico {
 
-    public Libro(String nombre, String editorial, Integer año_publicacion, String genero, String autor, String apellido, String tipo){
-        super(nombre,editorial, año_publicacion, genero, autor, apellido, tipo);
+    public Libro(String nombre, String editorial, Integer año_publicacion, String genero, String autorN, String autorA, String tipo){
+        super(nombre,editorial, año_publicacion, genero, autorN, autorA, tipo);
     }
 
     @Override
     public String generarReferencia() {
-        return getAutor() + ".(" + getAño()+ ")." + getNombre(nombre, apellido) + "."+ getEditorial();
+        return nombreReferencia(autorN, autorA) + "(" + getAño()+ ")." + getNombre() + "."+ getEditorial();
     }
 }
