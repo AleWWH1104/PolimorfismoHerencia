a50261e7c5ee66ec8f28fb498508ec9ecbfeac2b
Iris <irisale.ayala@gmail.com>
2023-10-05 07:55:44 -0600

    se agrego archivo csv al proyecto

--- DriverProgram.java
+++ DriverProgram.java
@@ -1,3 +1,21 @@
+import java.util.List;
+
 public class DriverProgram {
-    
+    public static void main(String[] args) {
+        // Ruta del archivo CSV
+        String rutaArchivo = "/Users/alejandraayala/Desktop/Proyectos_POOS2/PolimorfismoHerencia/Bibliografias.csv";
+
+        // Crear un objeto para leer el archivo existente
+        FileManager lector = new FileManager(rutaArchivo);
+        List<String[]> contenidoCSV = lector.leerCSV();
+
+        // Procesar el contenido del archivo CSV si es necesario
+        for (String[] fila : contenidoCSV) {
+            // Realizar operaciones con las filas, si es necesario
+            for (String elemento : fila) {
+                System.out.print(elemento + " ");
+            }
+            System.out.println();
+        }
+    }
 }
